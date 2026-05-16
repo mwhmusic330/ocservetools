073dab3911b216872ff4e42e1ca453175c0b6e7c
Michael <149101891+mwhmusic330@users.noreply.github.com>
2026-05-16 13:16:18 -0400

    tested session delete

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import requests
 IP='127.0.0.1'
 PORT='4096'
 ENDPOINT=f"http://{IP}:{PORT}"
-
+sid = 'ses_1ce40fd83ffe2aOEUvTjf35V4H' 
 def checke_health():
     r = requests.get(ENDPOINT + "/global/health")
     return r.json()
 
 def delete_session(sid):
     s = requests.delete(ENDPOINT + "/session/" + sid)
+    return s
 
 def create_session():
     c = requests.post(ENDPOINT + "/session")
@@ -19,6 +20,6 @@ def list_all_sessions():
     ls = requests.get(ENDPOINT + "/session")
     return ls.json()
 def main():
-    print(create_session())
+    print(list_all_sessions())
 if __name__ == "__main__":
     main()
