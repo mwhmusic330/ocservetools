d48b9adb50ce217311ae35f4a7e2896f2725f659
Michael <149101891+mwhmusic330@users.noreply.github.com>
2026-05-17 09:59:19 -0400

    make_request function added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 
 IP='127.0.0.1'
 PORT='4096'
-ENDPOINT=f"http://{IP}:{PORT}"
+BASE_URL=f"http://{IP}:{PORT}"
 parser = argparse.ArgumentParser(
     prog='ocs',
     description='cli tool for opencode server',
@@ -14,22 +14,26 @@ parser.add_argument('-s', '--status', action='store_true')
 parser.add_argument('-a', '--add', action='store_true')
 parser.add_argument('-d', '--delete', action='store_true')
 parser.add_argument('-l', '--list', action='store_true')
+
+def make_request(method: str ='GET', endpoint: str ='') -> dict:
+    r = requests.request(method, BASE_URL + endpoint)
+    return r.json()
+
 def check_health():
-    r = requests.get(ENDPOINT + "/global/health")
-    print(r.json())
+    r = make_request(endpoint="/global/health")
+    print(r)
 
 def delete_session(sid):
-    s = requests.delete(ENDPOINT + "/session/" + sid)
+    s = make_request('DELETE',"rsession/" + sid)
     return s
 
 def create_session():
-    c = requests.post(ENDPOINT + "/session")
-    return c.json()
+    c = requests.post('POST',"/session")
+    return c
 
 def list_all_sessions():
-    ls = requests.get(ENDPOINT + "/session")
-    data = ls.json()
-    for item in data:
+    ls = make_request(endpoint="/session")
+    for item in ls:
         print(f"{item["id"]}, {item["title"]}")
 
 def main():
@@ -37,9 +41,9 @@ def main():
     if args.status:
         check_health()
     if args.add:
-        print(checke_health())
+        print(create_session())
     if args.delete:
-        print(checke_health())
+        print(delete_session(sid))
     if args.list:
         list_all_sessions()
 
